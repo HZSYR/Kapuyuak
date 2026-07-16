bd7aed63b225fd45d445cc44a80aa841bb1a2d4c
HZSYR <hafizhassyura94@gmail.com>
2026-07-16 08:09:02 +0700

    Remove obsolete test PHP files

--- test_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('INDEX_FILE_LOCATION', 'd:/projek ojs/whileflood.xyz/index.php');
-chdir('d:/projek ojs/whileflood.xyz');
-require_once './lib/pkp/includes/bootstrap.inc.php';
-$req = Application::get()->getRequest();
-$user = $req->getUser();
-echo $user ? $user->getUsername() : 'No User';
