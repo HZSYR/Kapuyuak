7e48f311d03d38b44da601be6def15fe1d802d2d
HZSYR <hafizhassyura94@gmail.com>
2026-08-12 11:07:04 +0700

    fix(scan): remove .php extension regex from malware signature scanner

--- pages/api/scan.js
+++ pages/api/scan.js
@@ -86,7 +86,6 @@ export default async function handler(req, res) {
       /(?:169\.254\.169\.254|metadata\.google\.internal)/i,
       /\{php\}.*system\(.*\{(?:\/)?php\}/i,
       /\b(?:move_uploaded_file|file_put_contents)\s*\(\s*(?:\$_(?:FILES|POST|GET)|php:\/\/input)/i,
-      /\.phar|\.pht(?:ml)?|\.php[3-8]/i,
       /phar:\/\//i
     ];
     
