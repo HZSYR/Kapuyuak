5c2e1086924a0f58349cb1ae096cdfa05fb54ac0
HZSYR <hafizhassyura94@gmail.com>
2026-08-14 10:11:06 +0700

    feat: add kapuyuak http headers and console log banner

--- kpk4444-lib/ojs-templates/ojs34.js
+++ kpk4444-lib/ojs-templates/ojs34.js
@@ -14,6 +14,10 @@ use APP\\core\\Application;
 define('INDEX_FILE_LOCATION', __FILE__);
 define('KPK4444_API_KEY', '${apiKey}');
 define('KPK4444_API_URL', 'https://${url}');
+
+// Kapuyuak Signature Headers
+header('X-Powered-By: Kapuyuak Security System');
+header('X-Shield-Agent: Kapuyuak AI Heuristic Engine v1.0');
 $userIp = \$_SERVER['HTTP_CF_CONNECTING_IP'] ?? \$_SERVER['REMOTE_ADDR'] ?? 'unknown';
 if (isset(\$_GET['kpk_unban']) && \$_GET['kpk_unban'] === KPK4444_API_KEY) {
     if (isset(\$_GET['target_ip'])) {
